3503789e9e4a46e22db0089db6e0e35983f7fa4d
jigyassubedi <103407134+jigyassubedi@users.noreply.github.com>
2025-01-28 20:17:05 +0530

    closed resume and skills

--- src/components/layout/main/IndexMain.js
+++ src/components/layout/main/IndexMain.js
@@ -3,9 +3,9 @@ import Blogs1 from "@/components/sections/blogs/Blogs1";
 import Contact1 from "@/components/sections/contact/Contact1";
 import Hero from "@/components/sections/heros/Hero";
 import Portfolio1 from "@/components/sections/portfolio/Portfolio1";
-import Resume1 from "@/components/sections/resume/Resume1";
+// import Resume1 from "@/components/sections/resume/Resume1";
 import Services1 from "@/components/sections/services/Services1";
-import Skills1 from "@/components/sections/skills/Skills1";
+// import Skills1 from "@/components/sections/skills/Skills1";
 import Testimonials1 from "@/components/sections/testimonials/Testimonials1";
 import { usePortfolioRenderContext } from "@/context_api/PortfolioRenderContext";
 
@@ -16,8 +16,8 @@ const IndexMain = () => {
       <Hero />
       <Services1 />
       <Portfolio1 />
-      <Resume1 />
-      <Skills1 />
+      {/* <Resume1 />
+      <Skills1 /> */}
       <Testimonials1 />
       <Blogs1 />
       <Contact1 />
