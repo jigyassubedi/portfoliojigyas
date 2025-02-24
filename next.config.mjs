31d932e2485d76d4328d766178e0a60b1729257e
jigyassubedi <103407134+jigyassubedi@users.noreply.github.com>
2025-02-25 00:12:28 +0530

    hck

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  distDir: "out", // Ensures the build is placed in the 'out' directory
   images: {
-    unoptimized: true, // Required for GitHub Pages
+    unoptimized: true, // Required for GitHub Pages and static exports
   },
-  basePath: "/portfoliojigyas", // Important for GitHub Pages
+  basePath: "/portfoliojigyas", // Important for GitHub Pages deployment
+  reactStrictMode: true, // Keeps React in strict mode
 };
-module.exports = nextConfig;
+
+export default nextConfig;
